feec08376e06b23215d71fd608bf578267d44147
TChristensenDrumz <tchristensendrumz@gmail.com>
2021-01-16 20:52:32 -0800

    coded API and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import Header from "./components/Header";
+import User from "./components/User";
 
 function App() {
     return (
         <div> 
-           
+           <Header />
+           <User />
         </div>
     )
 }
